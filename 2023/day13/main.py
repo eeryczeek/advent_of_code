3e064936b4886ddb073f449cfc93ce9f1485bbcc
eeryczeek <eryk.ptaszynski@vp.pl>
2023-12-13 23:47:17 +0100

    last version

--- 2023/day13/main.py
+++ 2023/day13/main.py
@@ -1,31 +1,22 @@
-import itertools
-
-
 with open("2023/day13/input.txt") as f:
-    lines = f.readlines()
-
-patterns = [list(g) for k, g in itertools.groupby(
-    lines, key=lambda x: x != '\n') if k]
+    patterns = [list(line.split('\n'))
+                for line in ''.join(f.readlines()).split('\n\n')]
 
 
 def find_symmetry_index(pattern: list, already_found: int = None):
-    for row in range(1, len(pattern)):
-        if all(line1 == line2 for line1, line2 in zip(pattern[:row][::-1], pattern[row:])) and row != already_found:
-            return row
-    return None
+    return next((row for row in range(1, len(pattern)) if all(line1 == line2 for line1, line2 in zip(pattern[:row][::-1], pattern[row:])) and row != already_found), None)
 
 
 def alter_pattern(pattern: list):
-    for y in range(len(pattern)):
-        for x in range(len(pattern[y])):
-            changed_pattern = [[x for x in line] for line in pattern]
-            changed_pattern[y][x] = '#' if changed_pattern[y][x] == '.' else '.'
+    for y, line in enumerate(pattern):
+        for x, char in enumerate(line):
+            changed_pattern = [list(line) for line in pattern]
+            changed_pattern[y][x] = '#' if char == '.' else '.'
             yield ["".join(line) for line in changed_pattern]
 
 
 rows1, columns1, rows2, columns2 = 0, 0, 0, 0
-for i, pattern in enumerate(patterns):
-    pattern = [line.strip('\n') for line in pattern]
+for pattern in patterns:
     pattern_t = ["".join(line) for line in zip(*pattern)]
     rows1 += find_symmetry_index(pattern) or 0
     columns1 += find_symmetry_index(pattern_t) or 0
