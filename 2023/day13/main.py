f2b934c503ed8dc8ccb3211cf5152d40eb3523b8
eeryczeek <eryk.ptaszynski@vp.pl>
2023-12-13 23:07:26 +0100

    part2

--- 2023/day13/main.py
+++ 2023/day13/main.py
@@ -7,37 +7,38 @@ with open("2023/day13/input.txt") as f:
 patterns = [list(g) for k, g in itertools.groupby(
     lines, key=lambda x: x != '\n') if k]
 
-rows, columns = 0, 0
-for pattern in patterns:
-    pattern = [line.strip('\n') for line in pattern]
-    rows += sum(row for row in range(1, len(pattern)) if all(line1 ==
-                line2 for line1, line2 in zip(pattern[:row][::-1], pattern[row:])))
 
-    pattern = ["".join(line) for line in zip(*pattern)]
-    columns += sum(column for column in range(1, len(pattern)) if all(line1 ==
-                   line2 for line1, line2 in zip(pattern[:column][::-1], pattern[column:])))
-print(rows * 100 + columns)
+def find_symmetry_index(pattern: list, already_found: int = None):
+    for row in range(1, len(pattern)):
+        if all(line1 == line2 for line1, line2 in zip(pattern[:row][::-1], pattern[row:])) and row != already_found:
+            return row
+    return None
 
 
 def alter_pattern(pattern: list):
     for y in range(len(pattern)):
         for x in range(len(pattern[y])):
-            changed_pattern = [list(line) for line in pattern]
+            changed_pattern = [[x for x in line] for line in pattern]
             changed_pattern[y][x] = '#' if changed_pattern[y][x] == '.' else '.'
             yield ["".join(line) for line in changed_pattern]
 
 
-rows, columns = 0, 0
-for pattern in patterns:
+rows1, columns1, rows2, columns2 = 0, 0, 0, 0
+for i, pattern in enumerate(patterns):
     pattern = [line.strip('\n') for line in pattern]
+    pattern_t = ["".join(line) for line in zip(*pattern)]
+    rows1 += find_symmetry_index(pattern) or 0
+    columns1 += find_symmetry_index(pattern_t) or 0
     for altered_pattern in alter_pattern(pattern):
-        for row in range(1, len(altered_pattern)):
-            if all(line1 == line2 for line1, line2 in zip(altered_pattern[:row][::-1], altered_pattern[row:])):
-                rows += row
+        if find_symmetry_index(altered_pattern, find_symmetry_index(pattern)):
+            rows2 += find_symmetry_index(altered_pattern,
+                                         find_symmetry_index(pattern))
+            break
 
-        altered_pattern = ["".join(line) for line in zip(*altered_pattern)]
-        for column in range(1, len(altered_pattern)):
-            if all(line1 == line2 for line1, line2 in zip(altered_pattern[:column][::-1], altered_pattern[column:])):
-                columns += column
-
-print(rows * 100 + columns)
+        altered_pattern_t = ["".join(line) for line in zip(*altered_pattern)]
+        if find_symmetry_index(altered_pattern_t, find_symmetry_index(pattern_t)):
+            columns2 += find_symmetry_index(altered_pattern_t,
+                                            find_symmetry_index(pattern_t))
+            break
+print(rows1 * 100 + columns1)
+print(rows2 * 100 + columns2)
